2497d99253bfe537e86ef3f0144196faacfe1f69
carebears <carebears9561@gmail.com>
2017-06-14 10:59:57 +0530

    Initial Repository Wecare

---
+++ wecare/src/main/java/wecare/wecare/App.java
@@ -0,0 +1,13 @@
+package wecare.wecare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
