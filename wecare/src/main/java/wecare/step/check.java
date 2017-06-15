c2a550f928999c186de23fb2a72fe5c9f94cc667
ankush agrawal <ankush.agrawal@infostretch.com>
2017-06-15 17:02:39 +0530

    add application.properties

--- wecare/src/main/java/wecare/step/check.java
+++ wecare/src/main/java/wecare/step/check.java
@@ -7,7 +7,9 @@ public class check {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 Utils u=new Utils();
-System.out.println(u.readproperty("dbuser"));
+u.alertGetText();
+
+
 	}
 
 }
