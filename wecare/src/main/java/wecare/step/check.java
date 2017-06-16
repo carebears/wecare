91df14ea71bb7d6bbd618b640f71cac0bc6f4dc8
ankush agrawal <ankush.agrawal@infostretch.com>
2017-06-16 16:56:51 +0530

    add in check file

--- wecare/src/main/java/wecare/step/check.java
+++ wecare/src/main/java/wecare/step/check.java
@@ -1,15 +1,46 @@
 package wecare.step;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import wecare.utils.Utils;
 
 public class check {
+	ExtentReports Reports;
 
-	public static void main(String[] args) throws Exception {
+	
+	@Test
+	public void verifyReport() throws Exception {
 		// TODO Auto-generated method stub
-Utils u=new Utils();
-u.alertGetText();
+
+		 Reports=new ExtentReports(System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
+		 ExtentTest logger=Reports.startTest("passTest");
+		 logger.log(LogStatus.PASS, "Test Case Passed is passTest");
+		Reports
+        .addSystemInfo("Host Name", "SoftwareTestingMaterial")
+        .addSystemInfo("Environment", "Automation Testing")
+        .addSystemInfo("User Name", " anku");
+		
+		Utils u=new Utils();
+System.out.println(u.readproperty("dbuser"));;
+
 
 
 	}
+@AfterTest()
+public void ver(){
+	Reports.endTest(null);
+}
+@AfterSuite
+public void afterSuite(){
+	Reports.flush();
+	Reports.close();
+}
 
 }
